efcd41e326655e2df881423978df2178d038031c
maximinus <maximinus@gmail.com>
2021-08-07 22:32:03 +0100

    Added start of region data

---
+++ SPQR2/scripts/region.gd
@@ -0,0 +1,10 @@
+extends Node
+
+class_name Region
+
+func _init(name, city, culture, money, manpower, loyalty):
+	self.city = city
+	self.culture = culture
+	self.money = money
+	self.manpower = manpower
+	self.loyalty = loyalty
