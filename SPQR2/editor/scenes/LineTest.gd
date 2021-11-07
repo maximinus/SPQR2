e8bf12a2c5a2c92948b0d0b28d46efdabe398ab4
maximinus <maximinus@gmail.com>
2021-11-07 12:19:50 +0000

    Test code to render dotted line works

--- SPQR2/editor/scenes/LineTest.gd
+++ SPQR2/editor/scenes/LineTest.gd
@@ -10,17 +10,87 @@ func _ready() -> void:
 func _process(_delta) -> void:
 	if complete == true:
 		return
-	build_dotted_line()
+	#build_dotted_line()
 	build_curved_dotted()
 	complete = true
 
 func build_curved_dotted():
 	# calculate the length of the line
 	var all_points = $CurvedLine.points
-	var length = 0.0
+	var full_length = 0.0
 	for i in range(len(all_points) - 1):
-		length += all_points[i].distance_to(all_points[i + 1])
-	print(length)
+		full_length += all_points[i].distance_to(all_points[i + 1])
+	
+	# now we do essentially the same thing. We loop over all the lines
+	var index: int = 0
+	var start: Vector2 = all_points[index]
+	var end: Vector2 = all_points[index + 1]
+
+	var short_length = all_points[index].distance_to(all_points[index + 1])
+
+	var angle: float = atan2(end.y - start.y, end.x - start.x)
+	var offset: Vector2 = Vector2(DOTTED_LENGTH * cos(angle), DOTTED_LENGTH * sin(angle))
+	var start_point: Vector2 = start - Vector2(0.0, -1.0)
+	
+	var dots: float = full_length / DOTTED_LENGTH
+	var frac_dots: float = dots - floor(dots)
+	var start_length: float = frac_dots / 2.0
+	
+	start_point += offset * start_length
+	var end_point = start_point + offset
+	full_length -= start_length
+	short_length -= start_length
+	
+	var draw = true
+	while(true):
+		# keep calculating until length is exceeded, just as before
+		if draw == true:
+			var new_line: Line2D = Line2D.new()
+			new_line.add_point(start_point)
+			
+			if short_length < DOTTED_LENGTH:
+				end_point = start_point + (offset * (short_length / DOTTED_LENGTH))
+			
+			new_line.add_point(end_point)
+			new_line.width = 2.0
+			new_line.antialiased = true
+			$CurveTest.add_child(new_line)
+			draw = false
+		else:
+			draw = true
+		start_point += offset
+		end_point += offset
+		short_length -= DOTTED_LENGTH
+		
+		if short_length < 0:
+			# we have moved onto the next point
+			# invert the negative value we have
+			short_length *= -1
+			# move onto next points
+			index += 1
+			# have we moved too far?
+			if index >= (len(all_points) - 1):
+				return
+			# calculate the new offset
+			start = all_points[index]
+			end = all_points[index + 1]
+			angle = atan2(end.y - start.y, end.x - start.x)
+			offset = Vector2(DOTTED_LENGTH * cos(angle), DOTTED_LENGTH * sin(angle))				
+			start_point = start
+			# just need a small line to draw
+			end_point = start + (offset * (short_length / DOTTED_LENGTH))
+			# calculate new short length
+			short_length = all_points[index].distance_to(all_points[index + 1]) - short_length
+			# do we need to draw this? (logic has swapped due to loop above)
+			if draw == false:
+				var corner_line = Line2D.new()
+				corner_line.add_point(start_point)
+				corner_line.add_point(end_point)
+				corner_line.width = 2.0
+				corner_line.antialiased = true
+				$CurveTest.add_child(corner_line)
+			start_point = end_point
+			end_point += offset
 
 func build_dotted_line() -> void:
 	var start = $SampleLine.points[0]
