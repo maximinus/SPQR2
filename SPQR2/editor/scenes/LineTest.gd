ae72b143ebdeafcd0f9d10fa617ba93195081480
maximinus <maximinus@gmail.com>
2021-11-07 00:24:56 +0000

    Starting to make editor draw dotted lines for roads

---
+++ SPQR2/editor/scenes/LineTest.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+const DOTTED_LENGTH: float = 6.0
+
+var complete: bool = false
+
+func _ready() -> void:
+	pass
+
+func _process(_delta) -> void:
+	if complete == true:
+		return
+	build_dotted_line()
+	build_curved_dotted()
+	complete = true
+
+func build_curved_dotted():
+	pass
+
+func build_dotted_line() -> void:
+	var start = $SampleLine.points[0]
+	var end = $SampleLine.points[-1]
+	
+	# calculate length of line
+	var length: float = start.distance_to(end)
+	var angle: float = atan2(end.y - start.y, end.x - start.x)
+	var offset = Vector2(DOTTED_LENGTH * cos(angle), DOTTED_LENGTH * sin(angle))
+	var start_point = start - Vector2(0.0, -1.0)
+	
+	var dots: float = length / DOTTED_LENGTH
+	var frac_dots: float = dots - floor(dots)
+	var start_length: float = frac_dots / 2.0
+		
+	# first dot is just a small fraction, as defined above
+	# we start with a blank, so only adjust the line
+	start_point += (offset * start_length)
+	var end_point = start_point + offset
+	length -= start_length
+		
+	var draw = true
+	while(true):
+		# now create a new line
+		if draw == true:
+			var new_line: Line2D = Line2D.new()
+			new_line.add_point(start_point)
+			
+			if length < DOTTED_LENGTH:
+				end_point = start_point + (offset * start_length)
+
+			new_line.add_point(end_point)
+			new_line.width = 2.0
+			new_line.antialiased = true
+			$DottedLine.add_child(new_line)
+			draw = false
+		else:
+			draw = true
+		start_point += offset
+		end_point += offset
+		length -= DOTTED_LENGTH
+		if length < 0:
+			return
