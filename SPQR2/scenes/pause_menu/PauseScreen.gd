b52d2cca6e98158282ea646aaa5e671d76ec2c6a
maximinus <maximinus@gmail.com>
2021-12-11 13:34:37 +0000

    Added first piece of music and split effects into differing buses

--- SPQR2/scenes/pause_menu/PauseScreen.gd
+++ SPQR2/scenes/pause_menu/PauseScreen.gd
@@ -1,4 +1,14 @@
 extends CenterContainer
 
+# TODO:
+# scraping sound when the sliders move
+# click sound on button and checkbox
+# highlight sound on all objects
+# closes and clears up on OK click
+# stores values in game data
+# create some music in the world and play under a different bus from the sfx
+# sound changes as selection made, by changing the volume of the bus
+# show and hide come with fast fade in / out, also animates up / down very slightly
+
 func _ready():
 	pass
